8b1de49b819b646f59b4de05765e27133a81e4ed
dpb15 <brannerchinese@gmail.com>
2014-06-05 23:51:05 -0400

    initiate repo

---
+++ myvcs.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# myvcs.py
+# David Prager Branner
+# 20140605
+
+import difflib as D
+import os
+import argparse
+
+def main():
+    # QQQ This should happen only on initialization.
+    if '.myvcs' not in os.listdir():
+        os.mkdir('.myvcs')
+        ordinal = 0
+    else:
+        catalog = sorted([int(i) for i in os.listdir('.myvcs')])
+        print(catalog)
+        ordinal = catalog[-1] + 1
+    content = ''
+    with open('.myvcs/' + str(ordinal), 'w') as f:
+        f.write(content)
+
+if __name__ == '__main__':
+    main()
