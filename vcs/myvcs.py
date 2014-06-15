18f486aebb57deae2a45d80a280678649413f192
dpb15 <brannerchinese@gmail.com>
2014-06-15 18:47:37 -0400

    add myvcs working for step 1.2

--- vcs/myvcs.py
+++ vcs/myvcs.py
@@ -10,49 +10,60 @@ if sys.version_info.major < 3 and sys.version_info.minor < 3:
 import difflib as D
 import os
 import argparse
-import collections
+import shutil
 
 myvcs_ignore = {'.myvcs', '.myvcs.py.swp', '__pycache__', 'myvcs.py'}
 
 def main():
     # Get last backup dir and create directory for next backups, if needed.
-    next_backups = get_working_mvc_dir()
-    print(next_backups)
-    os.mkdir(next_backups)
+    next_backup_dir = get_working_mvc_dir()
+    print(next_backup_dir)
     #
     # Traverse subdirectories.
     current_dir = os.getcwd()
+    print('current_dir:', current_dir)
     walker = os.walk(current_dir)
     while True:
         try:
             present, next_dirs, files = next(walker)
         except StopIteration:
             break
-        next_dirs = collections.deque(next_dirs)
-        if next_dirs:
-            for old_dir in next_dirs:
-                new_dir = os.path.join(next_backups, old_dir)
-                os.mkdir(new_dir)
+        present = present.replace(current_dir, '').lstrip('/')
+        print('present: ', present)
+        if present in myvcs_ignore:
+            print('    continuing because {} in myvcs_ignore'.format(present))
+            continue
+        next_dirs[:] = [i for i in next_dirs if i not in myvcs_ignore]
+        files[:] = [i for i in files if i not in myvcs_ignore]
+        print('next_dirs: {}'.format(next_dirs))
+#        if next_dirs:
+#            for old_dir in next_dirs:
+#                if old_dir == 'lumpybones':
+#                    print('here')
+#                new_dir = os.path.join(next_backup_dir, old_dir)
+#                os.mkdir(new_dir)
         if files:
-            for f in files:
-                shutils.copy2(f, )
-
+            if present not in myvcs_ignore:
+                copy_to_dir = os.path.join(next_backup_dir, present)
+                if not os.path.exists(copy_to_dir):
+                    os.mkdir(copy_to_dir)
+                print('copy_to_dir:', copy_to_dir)
+                for f in files:
+                    print('    now trying {}'.format(f))
+                    shutil.copy2(os.path.join(present, f), copy_to_dir)
     print('Done.')
 
 def get_working_mvc_dir():
     # QQQ This should happen only on initialization.
     if '.myvcs' not in os.listdir():
         os.mkdir('.myvcs')
+        head = -1
     else:
         # Read .myvcs/HEAD and make sure that directory exists.
         if os.path.exists(os.path.join('.myvcs', 'HEAD')):
             with open(os.path.join('.myvcs', 'HEAD'), 'r') as f:
-                head = f.read()
-        else:
-            head = -1
-#            with open(os.path.join('.myvcs', 'HEAD'), 'w') as f:
-#                f.write(str(0))
-        if not (os.path.exists(os.path.join('.myvcs', str(head))) or 
+                head = int(f.read())
+        if not (os.path.exists(os.path.join('.myvcs', str(head))) or
                 os.path.isdir(os.path.join('.myvcs', str(head)))):
             print('Invalid value of .myvcs/HEAD: {}'.format(head))
     # Set HEAD to new working directory.
@@ -63,7 +74,7 @@ def get_working_mvc_dir():
     except Exception as e:
         print(e)
         sys.exit(1)
-    with open(os.path('.myvcs', 'HEAD'), 'w') as f:
+    with open(os.path.join('.myvcs', 'HEAD'), 'w') as f:
         f.write(str(head))
     return head_dir
 
