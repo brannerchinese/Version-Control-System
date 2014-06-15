2ebecd8ef229a6a5e92eb95639b92b7aa4db8c52
dpb15 <brannerchinese@gmail.com>
2014-06-15 15:34:15 -0400

    add revised myvcs for step 1.1

--- vcs/myvcs.py
+++ vcs/myvcs.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # myvcs.py
 # David Prager Branner
-# 20140614
+# 20140615
 
 import sys
 if sys.version_info.major < 3 and sys.version_info.minor < 3:
@@ -15,12 +15,60 @@ import collections
 myvcs_ignore = {'.myvcs', '.myvcs.py.swp', '__pycache__', 'myvcs.py'}
 
 def main():
-    # Get directory for current backups.
-    backup_dir = get_working_mvc_dir()
-    print(backup_dir)
-    os.mkdir(backup_dir)
-    # Get list of all files in present directory.
+    # Get last backup dir and create directory for next backups, if needed.
+    next_backups = get_working_mvc_dir()
+    print(next_backups)
+    os.mkdir(next_backups)
+    #
+    # Traverse subdirectories.
     current_dir = os.getcwd()
+    walker = os.walk(current_dir)
+    while True:
+        try:
+            present, next_dirs, files = next(walker)
+        except StopIteration:
+            break
+        next_dirs = collections.deque(next_dirs)
+        if next_dirs:
+            for old_dir in next_dirs:
+                new_dir = os.path.join(next_backups, old_dir)
+                os.mkdir(new_dir)
+        if files:
+            for f in files:
+                shutils.copy2(f, )
+
+    print('Done.')
+
+def get_working_mvc_dir():
+    # QQQ This should happen only on initialization.
+    if '.myvcs' not in os.listdir():
+        os.mkdir('.myvcs')
+    else:
+        # Read .myvcs/HEAD and make sure that directory exists.
+        if os.path.exists(os.path.join('.myvcs', 'HEAD')):
+            with open(os.path.join('.myvcs', 'HEAD'), 'r') as f:
+                head = f.read()
+        else:
+            head = -1
+#            with open(os.path.join('.myvcs', 'HEAD'), 'w') as f:
+#                f.write(str(0))
+        if not (os.path.exists(os.path.join('.myvcs', str(head))) or 
+                os.path.isdir(os.path.join('.myvcs', str(head)))):
+            print('Invalid value of .myvcs/HEAD: {}'.format(head))
+    # Set HEAD to new working directory.
+    head += 1
+    head_dir = os.path.join('.myvcs', str(head))
+    try:
+        os.mkdir(head_dir)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+    with open(os.path('.myvcs', 'HEAD'), 'w') as f:
+        f.write(str(head))
+    return head_dir
+
+def defunct():
+    # Get list of all files in present directory.
     directories = collections.deque([current_dir])
     while directories:
         next_dir = directories.popleft()
@@ -38,26 +86,6 @@ def main():
                 print('new directory:', f)
                 directories.append(full_path)
                 print(directories, all_files)
-
-def get_working_mvc_dir():
-    # QQQ This should happen only on initialization.
-    if '.myvcs' not in os.listdir():
-        os.mkdir('.myvcs')
-        ordinal = 0
-    else:
-        # Use loop rather than comprehension so as to allow try-block.
-        catalog = []
-        for i in os.listdir('.myvcs'):
-            try:
-                catalog.append(int(i))
-            except ValueError:
-                continue
-        catalog.sort()
-        print('catalog: {}'.format(catalog))
-        ordinal = catalog[-1] + 1
-    # Copy files into .myvcs/ordinal/ recursively.
-    working_dir = '.myvcs/' + str(ordinal)
-    return working_dir
 
 if __name__ == '__main__':
     main()
